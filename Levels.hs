11e0997372f07434a0f64ec3e3a45aba442d591d
János Illés <ijanos@gmail.com>
2010-02-12 22:59:40 +0100

    Loading levels from a file, separating parsing functions into a new module.

---
+++ Levels.hs
@@ -0,0 +1,47 @@
+module Levels (Level(lStart,lLength,lMap),Cell(Full,Empty),getLevels) where
+
+import Data.List
+import Data.Array
+import Control.Applicative
+
+data Level = Level {
+        lStart  :: (Int,Int),
+        lLength :: Int,
+        lMap    :: Array (Int,Int) Cell
+    } deriving (Show)
+
+data Cell = Full | Empty
+        deriving (Eq, Show)
+
+levelgroup _ chr = case chr of
+                     "" -> False
+                     _  -> True
+
+getLevels = do
+        lines <- lines <$> readFile "levels.txt"
+        let levels' = groupBy levelgroup lines
+        let levels'' = (map (dropWhile null) levels')
+        return $ map generate levels''
+        where generate level = Level lstart llength lmap
+                               where lstart = getStart level
+                                     lmap = convert level
+                                     llength = length [ x |  x@Full <- elems lmap]
+
+convert level = listArray ((1,1),(w,h)) [conv x | x <- concat.transpose $ level]
+    where h = length level
+          w = length $ head level
+          conv ch = case ch of
+              '#' -> Full
+              'S' -> Full
+              _   -> Empty
+
+getStart level = case foldl f (Left (1,1)) level of
+                    Right coords -> coords
+                    Left _      -> error "Cannot find start position"
+        where f = \acc str -> case acc of
+                                Left (x,y) -> case elemIndex 'S' str of
+                                                Just i -> Right (x',y)
+                                                          where x' = i+1
+                                                Nothing -> Left (x,y')
+                                                           where y' = y + 1
+                                Right a -> Right a
--- Main.hs
+++ Main.hs
@@ -3,6 +3,9 @@ module Main () where
 import Data.Array
 import Data.List
 import Graphics.UI.SDL as SDL
+import Control.Applicative
+
+import Levels
 
 cellsize  = 40
 border    = 1
@@ -13,53 +16,29 @@ cellcolor = SDL.Pixel 0x00999999
 hlcolor   = SDL.Pixel 0x00991111
 actcolor  = SDL.Pixel 0x00118505
 
-
-level1 = Level (3,4) 31 (mapToArray map2)
-map2 = ["#####    ",
-        "#   #    ",
-        "### ###  ",
-        "#######  ",
-        "#######  ",
-        "#        ",
-        "#########"]
-
-data Cell = Full | Empty
-        deriving (Eq, Show)
-
-data Level = Level {
-        lStart  :: (Int,Int),
-        lLength :: Int,
-        lMap    :: Array (Int,Int) Cell
-    } deriving (Show)
-
-mapToArray map = listArray ((1,1),(w,h)) [conv x | x <- concat.transpose $ map]
-    where h = length map
-          w = length $ head map
-          conv ch = case ch of
-              '#' -> Full
-              _   -> Empty
-
+main ::  IO ()
 main = do SDL.init [SDL.InitVideo]
           setVideoMode wwidth wheight 32 [HWSurface, DoubleBuf]
           setCaption "hKerixep" []
-          start (lMap level1)
+          level1 <- last <$> getLevels -- this just quick and dirty
+          start level1
 
-start board = do
-          screen<- SDL.getVideoSurface
-          mapM_ (draw screen cellcolor)
-                [(x,y)|((x,y),Full) <- assocs (lMap level1)]
-          draw screen actcolor (lStart level1)
-          SDL.flip screen
-          eventHandler [lStart level1]
+start level = do screen<- SDL.getVideoSurface
+                 mapM_ (draw screen cellcolor)
+                       [(x,y)|((x,y),Full) <- assocs (lMap level)]
+                 draw screen actcolor (lStart level)
+                 SDL.flip screen
+                 eventHandler [lStart level] level
 
+draw ::  Surface -> Pixel -> (Int, Int) -> IO Bool
 draw screen color (x,y) = SDL.fillRect screen (Just rect) color
         where rect = SDL.Rect (pos x) (pos y) fill fill
               pos n = padding + (n - 1) * cellsize
               fill = cellsize - border
 
-legalmove (x, y) clist@(c:cs) =
-                   if elem (x,y)  (indices (lMap level1))
-                      && (lMap level1 ! (x,y) == Full)
+legalmove (x, y) clist@(c:cs) level =
+                   if elem (x,y)  (indices (lMap level))
+                      && (lMap level ! (x,y) == Full)
                       && notElem (x,y) clist
                       && neighbour (x,y) c
                    then (True, (x,y):clist)
@@ -74,23 +53,23 @@ position curX curY = (x+1,y+1)
      where x = (curX-padding) `div` cellsize
            y = (curY-padding) `div` cellsize
 
-eventHandler :: [(Int, Int)] -> IO ()
-eventHandler clist = do
+eventHandler clist level = do
   e <- waitEvent
   case e of
     Quit -> return ()
     KeyDown (Keysym key _ _)   ->
         case key of
-            SDLK_q ->  return ()
-            _      ->  eventHandler clist
+            SDLK_q      -> return ()
+            SDLK_ESCAPE -> return ()
+            _           -> eventHandler clist level
     MouseMotion x y _ _ -> do let pos = position (fI x) (fI y)
-                              let (en,newclist) = legalmove pos clist
-                              if en then do screen<- SDL.getVideoSurface
+                              let (en,newclist) = legalmove pos clist level
+                              if en then do screen <- SDL.getVideoSurface
                                             draw screen hlcolor $ head clist
                                             draw screen actcolor pos
                                             SDL.flip screen
-                                            eventHandler newclist
-                                    else eventHandler clist
+                                            eventHandler newclist level
+                                    else eventHandler clist level
                               where fI = fromIntegral
-    MouseButtonDown _ _ _  -> start (lMap level1)
-    _ -> eventHandler clist
+    MouseButtonDown _ _ _  -> start level
+    _ -> eventHandler clist level

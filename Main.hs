6bc42b007546476c63c259bbd9e47748f35a6ade
János Illés <ijanos@gmail.com>
2010-02-14 19:01:45 +0100

    Properly load the first level and jump to the next one if the player solves it.
    And of course detect when there are no more levels.

--- Main.hs
+++ Main.hs
@@ -15,20 +15,27 @@ wheight   = 450
 cellcolor = SDL.Pixel 0x00999999
 hlcolor   = SDL.Pixel 0x00991111
 actcolor  = SDL.Pixel 0x00118505
+bgcolor   = SDL.Pixel 0x00111111
+
+data GameState = GameState {
+            levels :: [Level],
+            cList  :: [(Int,Int)]
+     } deriving (Show)
 
 main ::  IO ()
 main = do SDL.init [SDL.InitVideo]
           setVideoMode wwidth wheight 32 [HWSurface, DoubleBuf]
           setCaption "hKerixep" []
-          level1 <- last <$> getLevels -- this just quick and dirty
-          start level1
+          levels <- getLevels
+          start levels
 
-start level = do screen<- SDL.getVideoSurface
-                 mapM_ (draw screen cellcolor)
-                       [(x,y)|((x,y),Full) <- assocs (lMap level)]
-                 draw screen actcolor (lStart level)
-                 SDL.flip screen
-                 eventHandler [lStart level] level
+start levels = do screen<- SDL.getVideoSurface
+                  SDL.fillRect screen (Just (SDL.Rect 0 0 wwidth wheight)) bgcolor
+                  mapM_ (draw screen cellcolor)
+                        [(x,y)|((x,y),Full) <- assocs . lMap . head $ levels]
+                  draw screen actcolor $ lStart . head $ levels
+                  SDL.flip screen
+                  eventHandler $ GameState levels [lStart (head levels)]
 
 draw ::  Surface -> Pixel -> (Int, Int) -> IO Bool
 draw screen color (x,y) = SDL.fillRect screen (Just rect) color
@@ -53,7 +60,7 @@ position curX curY = (x+1,y+1)
      where x = (curX-padding) `div` cellsize
            y = (curY-padding) `div` cellsize
 
-eventHandler clist level = do
+eventHandler gamestate = do
   e <- waitEvent
   case e of
     Quit -> return ()
@@ -61,17 +68,21 @@ eventHandler clist level = do
         case key of
             SDLK_q      -> return ()
             SDLK_ESCAPE -> return ()
-            _           -> eventHandler clist level
+            _           -> eventHandler gamestate
     MouseMotion x y _ _ -> do let pos = position (fI x) (fI y)
-                              let (en,newclist) = legalmove pos clist level
+                              let (en,newclist) = legalmove pos (cList gamestate) currentlevel
                               if en then do screen <- SDL.getVideoSurface
-                                            draw screen hlcolor $ head clist
+                                            draw screen hlcolor $ head (cList gamestate)
                                             draw screen actcolor pos
                                             SDL.flip screen
-                                            if length newclist == lLength level
-                                             then do print "A WINNER IS YOU"
-                                             else eventHandler newclist level
-                                    else eventHandler clist level
+                                            if length newclist == lLength  currentlevel
+                                             then if null remaininglevels
+                                                   then print "a winner is you"
+                                                   else start remaininglevels
+                                             else eventHandler gamestate { cList = newclist }
+                                    else eventHandler gamestate
                               where fI = fromIntegral
-    MouseButtonDown _ _ _  -> start level
-    _ -> eventHandler clist level
+                                    remaininglevels = tail . levels $ gamestate
+                                    currentlevel = head . levels $ gamestate
+    MouseButtonDown _ _ _  -> start $ levels gamestate
+    _ -> eventHandler gamestate
